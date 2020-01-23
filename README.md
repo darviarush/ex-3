f56ce0a99af1a7cfdc5bdea751b2a36e26cb767d
Yaroslav O. Kosmina <kosmina.yaroslav@220-volt.ru>
2020-01-23 11:41:56 +0300

    master save

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Darviarush::Ex3 - It's new $module
+
+# SYNOPSIS
+
+    use Darviarush::Ex3;
+
+# DESCRIPTION
+
+Darviarush::Ex3 is ...
+
+# LICENSE
+
+Copyright (C) Yaroslav O. Kosmina.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Yaroslav O. Kosmina <kosmina.yaroslav@220-volt.ru>
---
+++ lib/Darviarush/Ex3.pm
@@ -0,0 +1,39 @@
+package Darviarush::Ex3;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Darviarush::Ex3 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Darviarush::Ex3;
+
+=head1 DESCRIPTION
+
+Darviarush::Ex3 is ...
+
+=head1 LICENSE
+
+Copyright (C) Yaroslav O. Kosmina.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yaroslav O. Kosmina E<lt>kosmina.yaroslav@220-volt.ruE<gt>
+
+=cut
+
